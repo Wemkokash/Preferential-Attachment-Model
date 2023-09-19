5d55c43ff53c6344e94e15cdf0164f1ed2b0d3b1
Robby Swenson <131019369+Wemkokash@users.noreply.github.com>
2023-09-19 08:51:01 -0500

    Update powerLaw.java
    big day

--- powerLaw.java
+++ powerLaw.java
@@ -1,1 +1,64 @@
-system.out.println("What up doe");
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class powerLaw {
+
+	public static void PAmodel(int numStartingNodes, int numIterations, int numNewEdges) {
+
+		int totalEdges = 0;
+		int totalNodes = numStartingNodes;  
+
+		//two Hash Maps, one that keeps track of which nodes link to each other, 
+		//one that keeps track of how many connections each node has
+		new HashMap edgeNumMap = HashMap();
+		new HashMap connectionMap = HashMap();
+
+//this is bad, find something else
+		//ArrayList to put in every new nodes connectionMap value
+		new ArrayList connections = ArrayList(numIterations * numNewEdges);
+
+		//add numStartingNodes into our HashMaps
+		for(i = 0; i < numStartingNodes; i++){
+
+			edgeNumMap.put(i, 0);
+			connectionMap.put(i, );
+		}
+
+
+		//for loop here
+		for(i = 0; i < numIterations; i++) {
+
+
+		}
+
+
+	};
+
+	
+
+	public static void main() {
+
+	};
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
