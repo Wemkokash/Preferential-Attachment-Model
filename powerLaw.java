6ba9f0a50f4f57745cce600571304c566131d6ba
Robby Swenson <131019369+Wemkokash@users.noreply.github.com>
2023-09-22 08:54:14 -0500

    Update powerLaw.java
    hee hee

--- powerLaw.java
+++ powerLaw.java
@@ -1,33 +1,55 @@
 import java.util.HashMap;
-import java.util.ArrayList;
+import java.util.LinkedList<E>;
 
 public class powerLaw {
 
-	public static void PAmodel(int numStartingNodes, int numIterations, int numNewEdges) {
+	public static void PAmodel(int numIterations) {
 
-		int totalEdges = 0;
-		int totalNodes = numStartingNodes;  
+		int totalEdges = 1;
+		int totalNodes = 2;
 
 		//two Hash Maps, one that keeps track of which nodes link to each other, 
 		//one that keeps track of how many connections each node has
 		new HashMap edgeNumMap = HashMap();
 		new HashMap connectionMap = HashMap();
 
-//this is bad, find something else
-		//ArrayList to put in every new nodes connectionMap value
-		new ArrayList connections = ArrayList(numIterations * numNewEdges);
+/*
+//is this necessary?
 
-		//add numStartingNodes into our HashMaps
-		for(i = 0; i < numStartingNodes; i++){
+		//LinkedList to put in every new node's connectionMap value
+		LinkedList<Integer> connections = new LinkedList<Integer>();
 
-			edgeNumMap.put(i, 0);
-			connectionMap.put(i, );
-		}
+*/
+		//both start with one connection (nodes 0 and 1)
+		edgeNumMap.put(0, 1);
+		edgeNumMap.put(1, 1);
+
+		//(x, y)
+		//in the connection map, for the node x link to 
+		//linked list with argument y in it 
+
+//not sure about this
+		connectionMap.put(0, new LinkedList<Integer>(1));
+		connectionMap.put(1, new LinkedList<Integer>(0));
 
 
-		//for loop here
+
+		//variable to keep track of what # node to add to model
+		//(since we already added some, the numStartingNodes)
+		int newNodeNum = numStartingNodes;
+
+		//IMPORTANT FOR LOOP here
+		//This is where we do the big boy work.
 		for(i = 0; i < numIterations; i++) {
 
+			//for each old node, calculate probability that the new node connects to it
+				//for loop for calculating probabilities
+
+			//random int for number of sum of numerators of probabilites
+
+			//add edges using newNodeNum and node calculated above
+
+			//iterate newNodeNum
 
 		}
 
@@ -43,22 +65,3 @@ public class powerLaw {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
